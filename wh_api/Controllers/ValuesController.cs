f3978c15d40ab5399709b90eba2a3149025df04b
Asus <capital.phyoethurakyaw@gmail.com>
2021-09-11 05:46:03 +0630

    Add project files.

---
+++ wh_api/Controllers/ValuesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using Newtonsoft.Json;
+using System.Data;
+using System.Text;
+
+namespace wh_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        static string resource = "D:/users.log";
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(GetActive());
+            //
+          //  return Ok(JsonConvert.SerializeObject(System.IO.File.ReadAllLines(resource)));
+          //  return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+        [HttpPost]
+        public IActionResult Post(JObject value)
+        {
+            return Ok(value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+        protected string GetActive()
+        {
+            DataTable dtstatus = new DataTable();
+            dtstatus.Columns.Add("PortType");
+            dtstatus.Columns.Add("HostIP");
+            dtstatus.Columns.Add("RemoteIP");
+            dtstatus.Columns.Add("Status");
+            var act = System.IO.File.ReadAllLines(resource);
+            if (act != null)
+            {
+                int ski = 0;
+                foreach (var s in act)
+                {
+                    ski++;
+                    if (ski > 4)
+                        //if (!s.Contains("Proto"))
+                        //{
+                        if (!s.Contains("127.0.0.1") && !s.Contains("TIME_WAIT"))
+                        {
+                            var prt = "";
+                            var hst = "";
+                            var rmt = "";
+                            var sts = "";
+                            try
+                            {
+                                var enc = Encoding.UTF8.GetString(Encoding.Default.GetBytes(s));
+                                prt = enc.Substring(0, 8);
+                                hst = enc.Replace(prt, "").TrimStart().Split(' ').First(); //enc.Substring(8, 22);
+                                rmt = enc.Replace(prt, "").Replace(hst, "").TrimStart().Split(' ').First(); //enc.Substring(22, 21);
+                                sts = enc.TrimEnd().Split(' ').Last();  // prt.Replace(prt, "").Replace(hst, "").Replace(rmt, "").Trim();
+                            }
+                            catch
+                            {
+                               // Response.Write("Error In reading");
+                            }
+                            dtstatus.Rows.Add(new object[] { prt, hst, rmt, sts });
+                        }
+                    //}
+                }
+            }
+            return DataTableToJSONWithJSONNet(dtstatus);
+        }
+        public string DataTableToJSONWithJSONNet(DataTable table)
+        {
+            string JSONString = string.Empty;
+            JSONString = JsonConvert.SerializeObject(table);
+            return JSONString;
+        }
+    }
+}
---
+++ wh_api/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace wh_api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(c => c.AddPolicy("AllowOrigin", Opt => Opt.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader())); ;
+            services.AddControllers();
+            services.AddControllersWithViews().AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling
+            = Newtonsoft.Json.ReferenceLoopHandling.Ignore).AddNewtonsoftJson(opt => opt.SerializerSettings.ContractResolver = new DefaultContractResolver());
+            //services.AddMvc(option => option.EnableEndpointRouting = false);
+            //services.AddControllers().AddNewtonsoftJson();
+            //services.AddCors(); // Make sure you call this previous to AddMvc
+            // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors(opt => opt.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            //          if (env.IsDevelopment())
+            //          {
+            //              app.UseDeveloperExceptionPage();
+            //          }
+            //          else
+            //          {
+            //              app.UseHsts();
+            //          }
+            //          app.UseCors(
+            //    options => options.WithOrigins("your link here").AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials()
+            //);
+
+            //          app.UseHttpsRedirection();
+            //          app.UseMvc();
+        }
+    }
+}
